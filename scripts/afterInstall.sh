c1d957cc1c077fcd63235a1ebb19bb5a59304ef9
Nishanth <manjunatha.ni@husky.neu.edu>
2020-03-09 16:49:49 -0400

    new commit-1-test

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/myaccountapp/
-python3 manage.py makemigrations
-python3 manage.py migrate
+cd /home/ubuntu/
+ls -al
+cat .bashrc
+#cd /home/ubuntu/myaccountapp/
+#python3 manage.py makemigrations
+#python3 manage.py migrate
