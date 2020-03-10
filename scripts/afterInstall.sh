8411ff4e6d0a739eb894be90c2fb079f7320725f
Nishanth <manjunatha.ni@husky.neu.edu>
2020-03-09 20:19:05 -0400

    new change-29

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 ../myaccountapp/manage.py makemigrations
+python3 ../myaccountapp/manage.py migrate
--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 manage.py runserver 0.0.0.0:8000
+python3 ../myaccountapp/manage.py runserver 0.0.0.0:8000
