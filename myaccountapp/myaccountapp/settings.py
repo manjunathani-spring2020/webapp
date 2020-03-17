bc6e5d031b7449448376f684bfc27f58c2b60090
Nishanth <manjunatha.ni@husky.neu.edu>
2020-03-17 04:28:45 -0400

    Assignment-8

--- myaccountapp/myaccountapp/settings.py
+++ myaccountapp/myaccountapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import logging.config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.relpath(__file__)))
@@ -161,3 +162,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'DB_HOST' in os.environ:
+    LOGGING_CONFIG = None
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '{asctime} {levelname} {module} {process:d} {thread:d} {message}',
+                'style': '{',
+            },
+        },
+        'filters': {
+            'require_debug_true': {
+                 '()': 'django.utils.log.RequireDebugTrue',
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename' : '/opt/aws/amazon-cloudwatch-agent/logs/csye6225.log',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'propagate': True,
+            }
+        }
+    })
