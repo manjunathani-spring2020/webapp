0b7f38a7d501cf8dd050724d6f8a749c9fadfcd6
Nishanth <manjunatha.ni@husky.neu.edu>
2020-04-03 05:14:42 -0400

    thread

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/myaccountapp/
-sudo nohup python3 -u manage.py runserver 0.0.0.0:8000 </dev/null >/dev/null 2>&1 &
+nohup python -u manage.py runserver 0.0.0.0:8000 </dev/null >/dev/null 2>&1 &
